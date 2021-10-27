a3d2945818d3165a7af1d13bddff758b1ed09219
leandro <leandrogguzman@hotmail.com>
2021-10-27 09:45:41 +0200

    cambio en la libreria mqtt

--- alarma/alarma.ino
+++ alarma/alarma.ino
@@ -7,7 +7,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 # include <ESP8266HTTPUpdateServer.h>
-#include <MQTT.h>
+#include <PubSubClient.h>
 #include <IotWebConf.h>
 #include <IotWebConfUsing.h>
 #include <EEPROM.h>
